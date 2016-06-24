e0c4446514d6745c3bb0a44b325fe4275d885fb6
quinnjn <quinn.neumiiller@getbraintree.com>
2016-06-24 14:27:47 -0700

    Accessing array and nested array

--- src/main/java/com/quinnjn/jsonpeek/JsonPeek.java
+++ src/main/java/com/quinnjn/jsonpeek/JsonPeek.java
@@ -45,6 +45,7 @@ public class JsonPeek {
      * - {"a":["b","c"]}. want(c). notation(a[1]). array(a,1)
      */
     private Object getProperty(String instruction) {
+        instruction = instruction.replaceAll("\\[", "").replaceAll("\\]", "");
         return getJsonObject(instruction);
     }
 
@@ -54,13 +55,50 @@ public class JsonPeek {
 
     private Object getJsonObject(String instruction) {
         JSONObject currentObject = jsonObject;
+        JSONArray currentArray = jsonArray;
+
         List<String> keys = new ArrayList<String>(Arrays.asList(instruction.split("\\.")));
         String lastKey = keys.remove(keys.size() -1);
 
         for (String key : keys) {
-            currentObject = currentObject.getJSONObject(key);
+            int keyInt = optInt(key, -1);
+
+            if (keyInt >= 0) {
+                try {
+                    currentArray = currentArray.getJSONArray(keyInt);
+                } catch (Exception e) {
+                    currentObject = currentArray.getJSONObject(keyInt);
+                }
+            } else {
+                try {
+                    currentArray = currentObject.getJSONArray(key);
+                } catch (Exception e) {
+                    currentObject = currentObject.getJSONObject(key);
+                }
+            }
         }
 
-        return currentObject.get(lastKey);
+        int keyInt = optInt(lastKey, -1);
+        if (keyInt >= 0) {
+            try {
+                return currentArray.get(keyInt);
+            } catch (Exception e) {
+                return currentArray.get(keyInt);
+            }
+        } else {
+            try {
+                return currentObject.get(lastKey);
+            } catch (Exception e) {
+                return currentObject.get(lastKey);
+            }
+        }
+    }
+
+    private int optInt(String s, int defaultVal) {
+        try {
+            return Integer.parseInt(s);
+        } catch (NumberFormatException notAnInt) {
+            return defaultVal;
+        }
     }
 }
