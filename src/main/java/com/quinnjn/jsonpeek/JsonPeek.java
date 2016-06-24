670158249c8db7e3ad32103b8720c23682126fb4
quinnjn <quinn.neumiiller@getbraintree.com>
2016-06-24 14:13:33 -0700

    Save the json array or object

--- src/main/java/com/quinnjn/jsonpeek/JsonPeek.java
+++ src/main/java/com/quinnjn/jsonpeek/JsonPeek.java
@@ -1,5 +1,7 @@
 package com.quinnjn.jsonpeek;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
@@ -8,22 +10,50 @@ import java.util.List;
 
 public class JsonPeek {
 
-    private final JSONObject json;
+    private final JSONArray jsonArray;
+    private final JSONObject jsonObject;
 
     public static JsonPeek from(String jsonString) {
         return new JsonPeek(jsonString);
     }
 
     private JsonPeek(String jsonString){
-        this.json = new JSONObject(jsonString);
+        JSONArray array = null;
+        JSONObject object = null;
+        try {
+            array = new JSONArray(jsonString);
+        } catch (JSONException notAnObject) {
+            try {
+                object = new JSONObject(jsonString);
+            } catch (JSONException notAnArray) {
+                throw new JSONException("Json is neither a JSONObject or JSONArray");
+            }
+        }
+        jsonArray = array;
+        jsonObject = object;
     }
 
     public String getString(String key) {
-        return (String) getJsonObject(key);
+        return (String) getProperty(key);
+    }
+
+    /**
+     * Takes a json dot notation string and divides it up into each layer to dive.
+     * Examples of how this should work.
+     * - {"a":{"b":{"c":"result"}}}. want(result). notation(a.b.c). array(a, b, c)
+     * - [[["a","b"]]]. want(b). notation([0][0][1]). array(0,0,1)
+     * - {"a":["b","c"]}. want(c). notation(a[1]). array(a,1)
+     */
+    private Object getProperty(String instruction) {
+        return getJsonObject(instruction);
+    }
+
+    private Object getProperty(List<String> keys) {
+        return null;
     }
 
     private Object getJsonObject(String instruction) {
-        JSONObject currentObject = json;
+        JSONObject currentObject = jsonObject;
         List<String> keys = new ArrayList<String>(Arrays.asList(instruction.split("\\.")));
         String lastKey = keys.remove(keys.size() -1);
 
--- src/test/java/com/quinnjn/jsonpeek/JsonPeekTest.java
+++ src/test/java/com/quinnjn/jsonpeek/JsonPeekTest.java
@@ -1,25 +1,37 @@
 package com.quinnjn.jsonpeek;
 
 import org.json.JSONException;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class JsonPeekTest {
 
+    @Rule
+    public ExpectedException constructorException = ExpectedException.none();
+
     @Test
-    public void from_returnsJsonPeek() {
+    public void from_withObject_returnsJsonPeek() {
         assertNotNull(JsonPeek.from("{}"));
     }
 
-    @Test(expected = JSONException.class)
+    @Test
+    public void from_withArray_returnsJsonPeek() {
+        assertNotNull(JsonPeek.from("[]"));
+    }
+
+    @Test()
     public void from_throwsExceptionWhenMalformedJson() {
+        constructorException.expect(JSONException.class);
+        constructorException.expectMessage("Json is neither a JSONObject or JSONArray");
         JsonPeek.from("nope");
     }
 
     @Test
-    public void getString_whenTopLevel_returnsString() {
+    public void getString_whenTopLevelObject_returnsString() {
         String jsonString = "{\"kString\":\"v\"}";
         String expected = "v";
 
@@ -28,11 +40,29 @@ public class JsonPeekTest {
     }
 
     @Test
-    public void getString_whenMultiNested_returnsString() {
+    public void getString_whenMultiNestedObject_returnsString() {
         String jsonString = "{\"top\":{\"mid\":{\"kString\":\"v\"}}}";
         String expected = "v";
 
         String actual = JsonPeek.from(jsonString).getString("top.mid.kString");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getString_whenArray_returnsString() {
+        String jsonString = "[\"v\"]";
+        String expected = "v";
+
+        String actual = JsonPeek.from(jsonString).getString("[0]");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getString_whenMultiNestedArray_returnsString() {
+        String jsonString = "[[[\"false\",\"v\"]]]";
+        String expected = "v";
+
+        String actual = JsonPeek.from(jsonString).getString("[0][0][1]");
         assertEquals(expected, actual);
     }
 }
