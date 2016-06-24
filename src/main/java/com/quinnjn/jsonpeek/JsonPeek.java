f884332b055af6c40da8a8c7da30fa4ccd760d56
quinnjn <quinn.neumiiller@getbraintree.com>
2016-06-24 13:10:54 -0700

    Access top level property

--- src/main/java/com/quinnjn/jsonpeek/JsonPeek.java
+++ src/main/java/com/quinnjn/jsonpeek/JsonPeek.java
@@ -1,4 +1,20 @@
 package com.quinnjn.jsonpeek;
 
+import org.json.JSONObject;
+
 public class JsonPeek {
+
+    private final JSONObject json;
+
+    public static JsonPeek from(String jsonString) {
+        return new JsonPeek(jsonString);
+    }
+
+    private JsonPeek(String jsonString){
+        this.json = new JSONObject(jsonString);
+    }
+
+    public String getString(String key) {
+        return json.getString(key);
+    }
 }
--- src/test/java/com/quinnjn/jsonpeek/JsonPeekTest.java
+++ src/test/java/com/quinnjn/jsonpeek/JsonPeekTest.java
@@ -1,14 +1,38 @@
 package com.quinnjn.jsonpeek;
 
+import org.json.JSONException;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class JsonPeekTest {
 
     @Test
-    public void sanityCheck() {
-        JsonPeek jsonPeek = new JsonPeek();
-        assertNotNull(jsonPeek);
+    public void from_returnsJsonPeek() {
+        assertNotNull(JsonPeek.from("{}"));
+    }
+
+    @Test(expected = JSONException.class)
+    public void from_throwsExceptionWhenMalformedJson() {
+        JsonPeek.from("nope");
+    }
+
+    @Test
+    public void getString_whenTopLevel_returnsString() {
+        String jsonString = "{\"kString\":\"v\"}";
+        String expected = "v";
+
+        String actual = JsonPeek.from(jsonString).getString("kString");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getString_whenMultiNested_returnsString() {
+        String jsonString = "{\"nested\":{\"kString\":\"v\"}}";
+        String expected = "v";
+
+        String actual = JsonPeek.from(jsonString).getString("nested.kString");
+        assertEquals(expected, actual);
     }
 }
